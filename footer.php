115cce91fdc4c8afa1fb4689d753e83d45ff9d5d
Sangwiss <guirec.talmo@gmail.com>
2014-07-09 09:19:39 +0200

    Debugage de l'index et du footer
    Debugage de l'index et du footer dont le css ne s'affichait pas.

--- footer.php
+++ footer.php
@@ -1,56 +1,70 @@
-    <div class="dmbs-footer">
-      <div class="row">
-          <div class="col-md-3">
-            <h5>Paramètres de localisation</h5>
-            <div class="language">
-                <select name="language">
-                    <option value="en">English</option>
-                    <option value="fr">Français</option>
-                    <option value="br">Brazilian / Portuguese</option>
-                </select>
-                <div class="icon"><i class="fa fa-globe"></i>
-                </div>
-            </div>
-            <div class="currency">
-                <select name="currency">
-                    <option value="EUR">€ - EUR</option>
-                    <option value="USD">$ - USD</option>
-                    <option value="GBP">£ - GBP</option>
-                    <option value="JPY">¥ - JPY</option>
-                </select>
-                <div class="icon">
-                    <i class="fa fa-usd"></i>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-3">
-            <h5 class="ng-binding">Société</h5>
-            <ul>
-                <li><a href="/aboutus" translate="" class="ng-scope">Qui sommes-nous ?</a></li>
-                <li><a href="/faq" translate="" class="ng-scope">FAQ</a></li>
-                <li><a href="/customs" translate="" class="ng-scope">Douanes</a></li><!--<li><a href="/tutorial">Tutoriel</a></li>-->
-                <li><a href="/community" translate="" class="ng-scope">Nos règles</a></li>
-                <li><a href="/terms" translate="" class="ng-scope">Condition Générales d'utilisation</a></li>
-            </ul>
-        </div>
-        <div class="col-md-2">
-            <h5 class="ng-binding">Suivez-nous</h5>
-            <ul>
-                <li><a target="_blank" href="https://twitter.com/worldcraze">Twitter</a></li>
-                <li><a target="_blank" href="https://facebook.com/worldcraze">Facebook</a></li>
-                <li><a target="_blank" href="https://plus.google.com/+Worldcraze">Google+</a></li>
-                <li><a target="_blank" href="http://laviedansunestartup.tumblr.com">Tumblr</a></li>
-                <li><a target="_blank" href="http://www.pinterest.com/worldcraze/">Pinterest</a></li>
-                <li><a target="_blank" href="https://www.linkedin.com/company/3514483">Linkedin</a></li>
-            </ul>
-        </div>
-        <div class="col-md-4">
-            <?php get_sidebar('footer-widget') ?>
-        </div>
-    </div>
+  <footer>
+    <div class="footer dbms-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-3">
+            <h5>{{ 'HOME_LOCALISATION_PARAMS' | translate }}</h5>
+            <form action="javascript;" method="POST">
+              <div class="language">
+                <select name="language" ng-model="selectedLanguage" ng-change="selectLanguage(selectedLanguage)">
+                  <option value="en">English</option>
+                  <option value="fr">Français</option>
+                  <option value="br">Brazilian / Portuguese</option>
+              </select>
+              <div class="icon"><i class="fa fa-globe"></i></div>
+              </div
+              >
+
+              <div class="currency" >
+                <select name='currency' ng-model="selectedCurrency" ng-change="selectCurrency(selectedCurrency)">
+                  <option value="EUR">&euro; - EUR</option>
+                  <option value="USD">$ - USD</option>
+                  <option value="GBP">£ - GBP</option>
+                  <option value="JPY">¥ - JPY</option>
+              </select>
+
+              <div class="icon"><i class="fa" ng-class="'fa-' + defaultToCurrency.code|lowercase"></i></div>
+          </div>
+      </form>
+
+      <div class="footer-lang">
+          <a href="/en">WorldCraze.com in English</a>
+      </div>
+
+  </div>
+
+  <div class="col-xs-3">
+    <h5>{{ 'HOME_SOCIETY' | translate }}</h5>
+
+    <ul>
+      <li><a href="/aboutus" translate>PANE_ABOUTUS</a></li>
+      <li><a href="/faq" translate>PANE_FAQ</a></li>
+      <li><a href="/customs" translate>PANE_CUSTOMS</a></li>
+      <!--<li><a href="/tutorial">Tutoriel</a></li>-->
+      <li><a href="/community" translate>PANE_COMMUNITY</a></li>
+      <li><a href="/terms" translate>PANE_TERMS</a></li>
+  </ul>
+
+</div>
+
+<div class="col-xs-2">
+    <h5>{{ 'HOME_FOLLOW_US' | translate }}</h5>
+
+    <ul>
+      <li><a target="_blank" href="https://twitter.com/worldcraze">Twitter</a></li>
+      <li><a target="_blank" href="https://facebook.com/worldcraze">Facebook</a></li>
+      <li><a target="_blank" href="https://plus.google.com/+Worldcraze">Google+</a></li>
+      <li><a target="_blank" href="http://laviedansunestartup.tumblr.com">Tumblr</a></li>
+      <li><a target="_blank" href="http://www.pinterest.com/worldcraze/">Pinterest</a></li>
+      <li><a target="_blank" href="https://www.linkedin.com/company/3514483">Linkedin</a></li>
+  </ul>
+</div>
+
+<div class="col-xs-4">
+    <div class="fb-like-box" data-href="https://www.facebook.com/WorldCraze" data-width="360" data-height="200" data-colorscheme="light" data-show-faces="true" data-header="false" data-stream="false" data-show-border="false"></div>
 </div>
 
 </div>
-<!-- end main container -->
-</body>
-</html>
+</div>
+</div>
+</footer>
--- index.php
+++ index.php
@@ -1,68 +1,60 @@
 <?php get_header(); ?>
 <body <?php body_class(); ?>>
-<?php get_template_part('template-part', 'facebook-init'); ?>
-<?php get_template_part('template-part', 'head'); ?>
-<?php get_template_part('template-part', 'topnav'); ?>
-<?php get_template_part('template-part', 'navigation'); ?>
-<!-- start content container -->
-<div class="row dmbs-content">
-
-  <?php //left sidebar ?>
-  <?php get_sidebar( 'left' ); ?>
-
-  <div class="col-md-<?php devdmbootstrap3_main_content_width(); ?> dmbs-main">
-
-            <?php // theloop
-            if ( have_posts() ) : while ( have_posts() ) : the_post();
+  <?php get_template_part('template-part', 'facebook-init'); ?>
+  <?php get_template_part('template-part', 'head'); ?>
+  <?php get_template_part('template-part', 'topnav'); ?>
+  <?php get_template_part('template-part', 'navigation'); ?>
+  <!-- start content container -->
+  <div class="row dmbs-content">
+    <?php //left sidebar ?>
+    <?php get_sidebar( 'left' ); ?>
+    <div class="col-md-<?php devdmbootstrap3_main_content_width(); ?> dmbs-main">
+      <?php // theloop
+      if ( have_posts() ) : while ( have_posts() ) : the_post();
 
                     // single post
-            if ( is_single() ) : ?>
+      if ( is_single() ) : ?>
+      <div <?php post_class(); ?>>
+        <div class="post">
+          <h3 class="page-header"><?php the_title() ;?></h3>
+        </div>
 
-            <div <?php post_class(); ?>>
-              <div class="post">
-                <h3 class="page-header"><?php the_title() ;?></h3>
-              </div>
-
-              <?php if ( has_post_thumbnail() ) : ?>
-              <?php the_post_thumbnail("large");?>
-              <div class="clear"></div>
-            <?php endif; ?>
-            <?php the_content(); ?>
-            <?php wp_link_pages(); ?>
-            <?php get_template_part('template-part', 'postmeta'); ?>
-            <?php comments_template(); ?>
-
-          </div>
-          <?php
+        <?php if ( has_post_thumbnail() ) : ?>
+        <?php the_post_thumbnail("large");?>
+        <div class="clear"></div>
+      <?php endif; ?>
+      <?php the_content(); ?>
+      <?php wp_link_pages(); ?>
+      <?php get_template_part('template-part', 'postmeta'); ?>
+      <?php comments_template(); ?>
+    </div>
+    <?php
                     // list of posts
-          else : ?>
-          <div <?php post_class(); ?>>
+    else : ?>
+    <div <?php post_class(); ?>>
+      <h3 class="page-header">
+        <a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'devdmbootstrap3' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
+      </h3>
+      <div class="center-img">
+        <?php if ( has_post_thumbnail() ) : ?>
+        <?php the_post_thumbnail( array(447,260) ) ?>
+        <?php the_post_thumbnail_caption(); ?>
+      <?php endif; ?>
+    </div>
+    <div class="paragraph-article">
+      <?php the_content(); ?>
+    </div>
+    <?php wp_link_pages(); ?>
+    <?php get_template_part('template-part', 'postmeta'); ?>
+    <?php  if ( comments_open() ) : ?> 
+    <a class="btn btn-success comment-article" href="<?php the_permalink(); ?>#comments"><?php comments_number(__('Leave a Comment','devdmbootstrap3'), __('One Comment','devdmbootstrap3'), '%' . __(' Comments','devdmbootstrap3') );?> <span class="glyphicon glyphicon-comment"></span></a>
+  <?php endif; ?>
+</div>
 
-            <h3 class="page-header">
-              <a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'devdmbootstrap3' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
-            </h3>
+<?php  endif; ?>
 
-            <div class="center-img">
-              <?php if ( has_post_thumbnail() ) : ?>
-              <?php the_post_thumbnail( array(447,260) ) ?>
-              <?php the_post_thumbnail_caption(); ?>
-            </div>
-          <?php endif; ?>
-          <div class="paragraph-article">
-            <?php the_content(); ?>
-          </div>
-          <?php wp_link_pages(); ?>
-          <?php get_template_part('template-part', 'postmeta'); ?>
-          <?php  if ( comments_open() ) : ?>
-          <div class="clear"></div> 
-          <a class="btn btn-success comment-article" href="<?php the_permalink(); ?>#comments"><?php comments_number(__('Leave a Comment','devdmbootstrap3'), __('One Comment','devdmbootstrap3'), '%' . __(' Comments','devdmbootstrap3') );?> <span class="glyphicon glyphicon-comment"></span></a>
-        <?php endif; ?>
-      </div>
-
-    <?php  endif; ?>
-
-  <?php endwhile; ?>
-  <?php posts_nav_link(); ?>
+<?php endwhile; ?>
+<?php posts_nav_link(); ?>
 <?php else: ?>
 
   <?php get_404_template(); ?>
@@ -72,8 +64,11 @@
 </div> <?php //end main content ?>
 
 <?php get_sidebar( 'right' ); ?>
-</div> <?php //end main container ?>
+</div> <?php //end row ?>
+
+</div>
 
 <?php get_footer(); ?>
+
 </body>
 </html>
