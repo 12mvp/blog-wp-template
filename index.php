f96d5299d709eeef8ab02609f8aaf81fa0b0cf83
Sangwiss <guirec.talmo@gmail.com>
2014-07-09 10:51:18 +0200

    Ajout de l'option share

--- index.php
+++ index.php
@@ -70,5 +70,13 @@
 
 <?php get_footer(); ?>
 
+<?php
+/* Always have wp_footer() just before the closing </body>
+* tag of your theme, or you will break many plugins, which
+* generally use this hook to reference JavaScript files.
+*/
+wp_footer();
+?>
+
 </body>
 </html>
--- template-part-postmeta.php
+++ template-part-postmeta.php
@@ -12,6 +12,7 @@
                 <span class="glyphicon glyphicon-tags"></span> 
                 <?php the_tags(); ?>
             <?php endif; ?>
+            <div class="fb-like" data-href="<?php the_permalink(); ?>" data-layout="button_count" data-action="like" data-show-faces="false" data-share="true"></div>
         </div>
     </div>
 <?php endif; ?>
